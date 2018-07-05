7c4870f07517260ac6004e1cdb24b4b3990c5dbb
vivekmr <vivek.kumar@happay.in>
2018-07-05 10:53:42 +0530

    Added TF files

--- aws-provider.tf
+++
@@ -1,8 +0,0 @@
-# Configure the AWS Provider
-provider "aws" {
-  version = "~> 1.25"
-#  access_key = "${var.aws_access_key}"
-#  secret_key = "${var.aws_secret_key}"
-#  region     = "us-east-1"
-}
-
